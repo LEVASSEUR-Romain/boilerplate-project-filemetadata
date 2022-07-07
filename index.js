6f5ad56cf9230f2a41a9b39ee70d458725e8fb56
Romain <romainlev@gmail.com>
2022-07-07 14:56:32 +0200

    freedcode camp exo5

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var express = require("express");
-var cors = require("cors");
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -14,4 +14,17 @@ app.get("/", function (req, res) {
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Your app is listening on port " + port);
+});
+
+// exercices
+// multer
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
 });
